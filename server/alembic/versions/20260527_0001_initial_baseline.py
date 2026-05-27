cae48aeb4952e4f3465e6e1ee59b6c3686fd414c
rujulw <wrujul26@gmail.com>
2026-05-27 12:23:10 +0530

    [impl] add SQLAlchemy async engine, session factory, and Alembic migration baseline

---
+++ server/alembic/versions/20260527_0001_initial_baseline.py
@@ -0,0 +1,21 @@
+"""initial baseline
+
+Revision ID: 20260527_0001
+Revises:
+Create Date: 2026-05-27 00:00:00
+
+"""
+
+
+revision = "20260527_0001"
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
